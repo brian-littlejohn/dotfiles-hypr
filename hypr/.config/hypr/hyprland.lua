4d1a840a4adf60179dd207dff6fbced7714f69a6
Brian Littlejohn <brian@thelittlejohns.us>
2026-09-17 14:02:17 -0500

    Added Diskie Startup

--- hypr/.config/hypr/hyprland.lua
+++ hypr/.config/hypr/hyprland.lua
@@ -654,6 +654,7 @@ hl.on("hyprland.start", function()
     hl.exec_cmd("hyprpaper")
     hl.exec_cmd("nextcloud")
     hl.exec_cmd("otp-tui agent run --on-lock-cmd 'pkill -RTMIN+8 waybar'")
+    h1.exec_cmd("diskie --tray")
 end)
 
 hl.on("config.reloaded", function()
